fb68a21c7a0cd202631a85b8bdb2f5344ae57441
achintyakaushaljha <achintya.kaushaljha@infra.market>
2023-08-26 17:54:02 +0530

    lifting up state

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stat />
     </div>
   );
@@ -21,13 +25,16 @@ function Logo() {
   return <h1>🚀Far Away🚀</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    onAddItems(newItem);
+
     setDescription("");
     setQuantity(1);
   }
@@ -56,11 +63,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list ul">
       <ul className="list">
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
